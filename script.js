f6f01368057a4ac3a3630c3e478143d489c7ad19
1stmuse <olysegs@gmail.com>
2020-12-05 08:35:02 +0100

    added congratulation message

--- script.js
+++ script.js
@@ -20,28 +20,35 @@ const submit =() =>{
         return
     }
     let difference = Math.abs(randomNumber - inputValue.value)
-    if(difference >80 && difference < 99){
+    if(difference >=80 && difference <=99){
         range.style.width = `${10}%`
+        range.style.backgroundColor = 'darkgrey'
         temStatus.textContent = 'you are getting colder'
     }
     if(difference >50 && difference < 80){
         range.style.width = `${30}%`
+        range.style.backgroundColor = 'grey'
         temStatus.textContent = 'you are getting cold'
+        
     }
-    if(difference >30 && difference <50){
+    if(difference >30 && difference <=50){
         range.style.width = `${50}%`
+        range.style.backgroundColor = 'orange'
         temStatus.textContent = 'you are cool'
     }
-    if(difference >10 && difference <30){
+    if(difference >10 && difference <=30){
         range.style.width = `${70}%`
+        range.style.backgroundColor = 'orangered'
         temStatus.textContent = 'you are getting hot'
     }
-    if(difference >= 1 && difference <10){
+    if(difference >= 1 && difference <=10){
         range.style.width = `${90}%`
+        range.style.backgroundColor = 'red'
         temStatus.textContent = 'you are getting hotter'
     }
     if(difference === 0){
         range.style.width = `${100}%`
+        temStatus.textContent = 'congratulations you guessed'
     }
     inputValue.value = ''
     displayMessage.style.display = 'none'
